37e8a94e593a141f5c5479268e9d35e9e0abea7b
mb <michaelsenapon@gmail.com>
2023-05-21 16:28:32 +0000

    Fix edge cases error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	unit = (n % 10);
-	if (unit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, unit);
-	else if (unit == 0)
+	if (unit == 0)
 		printf("Last digit of %i is %i and is zero\n", n, unit);
+	else if (unit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, unit);
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, unit);
 	return (0);
