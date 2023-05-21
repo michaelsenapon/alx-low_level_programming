e9719d5a405853f69a7b7947399fd3c5d93aeca8
mb <michaelsenapon@gmail.com>
2023-05-21 17:50:19 +0000

    Print all single digit numbers starting from 0 and
    no use of any variable of type var
    use only putchar twice

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 48;
+
+	for (; digit <= 57; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
