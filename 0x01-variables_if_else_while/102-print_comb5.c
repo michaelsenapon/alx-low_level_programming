3bda8b776f551b301dcb8cf803bb53272b1e82bc
mb <michaelsenapon@gmail.com>
2023-05-22 20:12:32 +0000

    Fix documentation style error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,46 +12,37 @@ int main(void)
 	int digit_c;
 	int digit_d;
 
-	while (digit_a <= 57)
+	for (; digit_a <= 57; digit_a++)
 	{
 		digit_b = 48;
 		digit_c = digit_a;
 		digit_d = 49;
-		
-		while (digit_b <= 56)
+		for (; digit_b <= 56; digit_b++)
 		{
 			digit_c = digit_a;
 			digit_d = digit_b + 1;
-
-			while (digit_c <= 57)
+			for (; digit_c <= 57; digit_c++)
 			{
 				if (digit_a == digit_c)
 					digit_d = (digit_b + 1);
 				else
 					digit_d = 48;
-
-				while (digit_d <= 57)
+				for (; digit_d <= 57; digit_d++)
 				{
 					putchar(digit_a);
 					putchar(digit_b);
 					putchar(' ');
 					putchar(digit_c);
 					putchar(digit_d);
-
 					if (!(digit_a == 57 && digit_b == 56))
 					{
 						putchar(',');
 						putchar(' ');
 					}
-					digit_d++;
 				}
-				digit_c++;
 			}
-			digit_b++;
 		}
-		digit_a++;
 	}
 	putchar('\n');
 	return (0);
 }
-
