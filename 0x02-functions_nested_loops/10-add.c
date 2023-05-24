2b4c01f4b5376642f1b149e5bbcfdb1e1d0d5a1e
mb <michaelsenapon@gmail.com>
2023-05-24 16:55:59 +0000

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and return the result
+ * @i: first parameter for addition
+ * @j: second parameter for addition
+ *
+ * Return: Sum of the parameters
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int c);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void jack_bauer(void);
 void times_table(void);
