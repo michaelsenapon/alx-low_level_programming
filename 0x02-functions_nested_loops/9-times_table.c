b4237b352c2244b53377cdb9c2a1ad867931a26d
mb <michaelsenapon@gmail.com>
2023-05-24 21:38:21 +0000

    Fix betty Doc-style error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,14 +5,12 @@
  */
 void times_table(void)
 {
-	int i = 0;
-	int res;
-	int tens;
-	int unit;
+	int i = 0, res, tens, unit;
 
 	for (; i <= 9; i++)
 	{
 		int j = 0;
+
 		for (; j <= 9; j++)
 		{
 			res = (i * j);
