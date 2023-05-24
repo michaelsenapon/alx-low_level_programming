f1b200b623ee8e77371e12f904bdb21ff7cc2842
michaelsenapon <michaelsenapon@gmail.com>
2023-05-24 10:03:13 +0000

    Fix betty doc-style errors

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,10 +6,10 @@
  *
  * Return: Absolute value
  */
- int _abs(int c)
- {
-	 if (c < 0)
-		 return (c * -1);
-	 else
-		 return (c);
- }
+int _abs(int c)
+{
+	if (c < 0)
+		return (c * -1);
+	else
+		return (c);
+}
