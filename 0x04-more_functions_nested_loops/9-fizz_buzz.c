419d2d6a97c2261b162b61078a87d28b3700b8b8
mb <michaelsenapon@gmail.com>
2023-05-25 19:25:03 +0000

    Remove last empty space from output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -25,13 +25,15 @@ void fizz_buzz(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if (x % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (x % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (x % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", x);
+			printf("%d", x);
+		if (x != 100)
+			printf(" ");
 	}
 	printf("\n");
 }
