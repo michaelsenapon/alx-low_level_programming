ed549b26ab98b00be39e1cdf41a65033671ce2c6
mb <michaelsenapon@gmail.com>
2023-05-25 16:40:13 +0000

    Fix Betty Doc-style error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers(void) - Prints 10 times the numbers, from 0 to 14
+ * more_numbers - Prints 10 times the numbers, from 0 to 14
  */
 void more_numbers(void)
 {
