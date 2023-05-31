56e31070b4f57ad9feb9152a45b0b5147dbcdf17
michaelsenapon <michaelsenapon@gmail.com>
2023-05-31 12:57:17 +0000

    Print a selected number of elements from an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Print n elements of an array of integers
+ * @a: Pointer to integer variable
+ * @n: Number of integers
+ */
+void print_array (int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
