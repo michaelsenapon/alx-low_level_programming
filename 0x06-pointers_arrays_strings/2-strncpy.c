029ed3d0ca4e3e53f9304b15b0a7e51ecb5c3e13
mb <michaelsenapon@gmail.com>
2023-06-01 21:22:38 +0000

    Copies a string into another string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies n number of bytes into another string
+ * doesn't copy bytes that appear after Null bytes 
+ * @dest: Pointer to the String to be copied to
+ * @src: Pointer to the string to be copied from
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to the Destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (src[i] == '\0')
+		{
+			for (; i < n; i++)
+			{
+				dest[i] = '\0';
+			}
+			break;
+		}
+		else
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
