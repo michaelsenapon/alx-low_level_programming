c3bd311f9f57a9cd2409e4371c2aae20ab997a92
mb <michaelsenapon@gmail.com>
2023-06-01 16:41:22 +0000

    Concatenate two strings copying n number of chars from source string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings taking n number of characters from src string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters to be copied
+ *
+ * Return: Pointer to the resulting string <dest>
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int i;
+
+	while (dest[destlen])
+		destlen++;
+
+	for(i = 0; i < n && src[i] != '\0'; i++)
+		dest[destlen + i] = src[i];
+	dest[destlen + i] = '\0';
+
+	return (dest);
+}
