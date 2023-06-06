1c713e8983e6b84c5322b3b994881e0f5e8a0000
mb <michaelsenapon@gmail.com>
2023-06-06 03:51:03 +0000

    Search a string for any of a set of bytes like strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that locates a substring
+ * @haystack: String to be searched
+ * @needle: String that contains a substring
+ *
+ * Pointer to the beggining of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i+++
+	}
+	return (0);
+}
