9033684697d58546ba66a812e9fc886e064608f3
mb <michaelsenapon@gmail.com>
2023-06-11 13:59:44 +0000

    Use array directly instead of using pointer to access array elements

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Calculate the min number of coins to make change for money
@@ -10,56 +11,33 @@
  */
 int main(int argc, char **argv)
 {
-	int count = 0, cents;
+	int count = 0, cents, i;
 	int reg[5] = {25, 10, 5, 2, 1};
-	int *ptr = reg;
-
+/*
+ *	int *ptr = reg;
+*/
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = _atoi2(argv[1]);
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 	}
-	while (*ptr)
+	for (i = 0; i < 6; i++)
 	{
 		if (cents == 0)
 		{
 			printf("%d\n", count);
 			return (count);
 		}
-		if (cents >= *ptr)
+		if (cents >= reg[i])
 		{
-			count += cents / *ptr;
-			cents %= *ptr;
-			ptr++;
-			continue;
+			count += cents / reg[i];
+			cents %= reg[i];
 		}
-		ptr++;
 	}
 	return (0);
-}
-
-/**
- * _atoi2 - Function that converts string to integer
- * @s: String to convert
- *
- * Return: the int result or -1 if non digit character is found
- */
-int _atoi2(char *s)
-{
-	int num = 0;
-
-	while (*s)
-	{
-		if (*s >= 48 && *s <= 57)
-			num = (num * 10) + (*s - 48);
-		else
-			return (-1);
-		s++;
-	}
-	return (num);
 }
