cdf6074f504d2401c96fd20690a09751ecd2b3c4
mb <michaelsenapon@gmail.com>
2023-06-14 22:16:16 +0000

    Re-style code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,22 +17,16 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	grid = malloc(sizeof(int *) * height);
-
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-
 		if (grid[i] == NULL)
 		{
 			for (; i >= 0; i--)
-			{
 				free(grid[i]);
-			}
 			free(grid);
 			return (NULL);
 		}
