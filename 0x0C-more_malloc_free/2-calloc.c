1107e2694c0723481fcefdb93cb6ce6b268b1a14
mb <michaelsenapon@gmail.com>
2023-06-18 08:25:06 +0000

    Separate conditions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,9 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	/* Allocate memory using malloc */
 	ptr = malloc(nmemb * size);
+	
+	if (ptr == NULL)
+		return (NULL);
 
 	/* Check if nmemb OR size is 0 or malloc fails */
-	if ((nmemb == 0 || size == 0) || ptr == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	return (ptr);
 }
