fd6f55bd0bcc26e5fbbf270223644a98ed487737
mb <michaelsenapon@gmail.com>
2023-06-18 07:52:48 +0000

    Allocate memory for an array using malloc but imitate calloc implementation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Function allocate memory for an array, using malloc
+ * @nmemb: Number of elements contained in the array
+ * @size: The size of each element contained in the array
+ *
+ * Return: void pointer to the array or NULL if failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	/* Allocate memory using malloc */
+	ptr = malloc(nmemb * size);
+
+	/* Check if nmemb OR size is 0 or malloc fails */
+	if ((nmemb == 0 || size == 0) || ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
