72cf973d22921ff60f13e613f6d2ccbcf21a9af3
mb <michaelsenapon@gmail.com>
2023-06-20 04:38:49 +0000

    Print a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - Structure name tag
- * @name: Name of dog
- * @age: Age of dog
- * @owner: Name of owner
+ * struct dog - a structure type with elements defined
+ * @name: pointer to element that hold dog's name
+ * @age: element that holds age
+ * @owner: pointer to element that holds owner's name
  */
 struct dog
 {
@@ -13,6 +13,14 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif /* _DOG_H_ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
