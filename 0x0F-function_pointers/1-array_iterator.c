17c36769d1a3d434ea81add3ecd35a5b55f6ada8
mb <michaelsenapon@gmail.com>
2023-06-22 04:26:49 +0000

    Check for NULL pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Exe a function passed as parameter on each array elem
+ * @array: array to be worked on
+ * @size: size of array
+ * @action: Function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
