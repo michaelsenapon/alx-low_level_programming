f25d87ccfbe1f1d31179bac461f16da77835d425
mb <michaelsenapon@gmail.com>
2023-06-22 12:06:35 +0000

    Add edge case for when n == 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
+	if (n == 0)
+		return;
 	if (separator == NULL || ap == NULL)
 		return;
 	va_start(ap, n);
