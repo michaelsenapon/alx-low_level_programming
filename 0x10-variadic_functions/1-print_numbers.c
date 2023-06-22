bb2c54e660aa252c336fdba23da5fd8a636e6214
mb <michaelsenapon@gmail.com>
2023-06-22 11:41:52 +0000

    Print numbers passed into the variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Function that print numbers passed as variadic param
+ * @separator: String to be printed between numbers
+ * @n: Number of integers passed to the function
+ * @...: Elipses (Rest of the parameter)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (separator == NULL)
+		return;
+	va_start(ap, n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(ap);
+}
