66157661e2f87d26df868bd832eff83c46b0e196
mb <michaelsenapon@gmail.com>
2023-06-22 11:09:31 +0000

    Fix betty style error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,14 +4,16 @@
 /**
  * sum_them_all - Function that returns the sum of all its parameters
  * @n: Number of parameters passed
- * @...: elipses
+ * @...: elipses;
+ *
+ * Return: The sum of parameters or 0 if n == 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
 	va_list ap;
-	
+
 	if (n == 0)
 		return (0);
 
