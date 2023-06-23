8f35a14d8e1365329351210bda94bf4f9f531eb1
mb <michaelsenapon@gmail.com>
2023-06-23 04:55:58 +0000

    Print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,15 +20,8 @@ void print_all(const char * const format, ...)
 	i = 0, n = 0;
 	while (format && format[i] != '\0')
 	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && n)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
+		print_separator(format[i], n);
+
 		switch (format[i])
 		{
 		case 'c':
@@ -55,3 +48,24 @@ void print_all(const char * const format, ...)
 	printf("\n");
 	va_end(list);
 }
+/**
+ * print_separator - Function print separator
+ * @c: Character to observe
+ * @n: Number to observe
+ */
+void print_separator(char c, int n)
+{
+	int j = 0;
+	char id[] = "cifs";
+
+	while (id[j])
+	{
+		if (c == id[j] && n)
+		{
+			printf(", ");
+			break;
+		}
+		j++;
+	}
+}
+
