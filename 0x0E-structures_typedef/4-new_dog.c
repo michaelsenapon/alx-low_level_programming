61f7b48dafaffd525e7a10b796b139d105edffec
mb <michaelsenapon@gmail.com>
2023-06-24 23:08:29 +0000

    Create a new variable of type structure and initialize elements

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Function that creates a new structure of type dog_t
+ * @name: Pointer to string for name of dog
+ * @age: Age of dog
+ * @owner: Name of owner
+ *
+ * Return: Pointer to a structure of type dog_t
+ * NULL if the function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
