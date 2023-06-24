03c09cca036a9a06791bb9d05e92ae553be0aae8
mb <michaelsenapon@gmail.com>
2023-06-24 23:19:12 +0000

    Rearrange code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,17 +12,16 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *dog = NULL;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
-	if (name == NULL || owner == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
