a430ad0112cdb612bbda049a854d667f46407375
mb <michaelsenapon@gmail.com>
2023-06-25 12:21:51 +0000

    Correct typo

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,10 +23,8 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-		{
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (0);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
   */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	ptr = get_op_func(argv[2]);
 
 	if (ptr == NULL)
 	{
@@ -27,6 +27,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
