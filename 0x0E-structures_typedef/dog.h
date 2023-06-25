0caeef6f0d241864f0d7a0e8a34b4db4589aacdd
mb <michaelsenapon@gmail.com>
2023-06-25 00:56:06 +0000

    Update Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,8 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *);
+void _strcpy(char *, char *);
 void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
