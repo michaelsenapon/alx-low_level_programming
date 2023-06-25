8d7f1f7cd255c24db349252b5308def11274219f
mb <michaelsenapon@gmail.com>
2023-06-25 00:09:30 +0000

    Allocate more memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Function that frees memory allocated for a structure
+ * @d: Pointer to allocated memory for a structure aliased as dog_t
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
