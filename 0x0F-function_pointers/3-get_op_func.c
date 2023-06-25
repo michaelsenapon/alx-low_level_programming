86d839597badcfb2b44e7a1c9da406e441181b35
mb <michaelsenapon@gmail.com>
2023-06-25 11:56:34 +0000

    Re-adjust return value

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 		{
@@ -29,5 +29,5 @@ int (*get_op_func(char *s))(int, int)
 		}
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
