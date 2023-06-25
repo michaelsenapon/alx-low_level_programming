2d48034a2db7d3e3ee18019ff3dbb5ff2fd5b442
mb <michaelsenapon@gmail.com>
2023-06-25 00:55:42 +0000

    Allocate memory and copy strings into allocated memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,21 +22,60 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char *));
+	dog->name = malloc(sizeof(char) * _strlen(name));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char *));
+	dog->owner = malloc(sizeof(char) * _strlen(owner));
 	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
-	dog->name = name;
+	_strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = owner;
+	_strcpy(dog->owner, owner);
 	return (dog);
+}
+
+/**
+ * _strlen - Compute the length of string
+ * @str: String to compute
+ *
+ * Return: Number of length
+ */
+int _strlen(char *str)
+{
+	int i;
+	
+	if (str != NULL)
+	{
+		for (i = 0; str[i]; i++)
+			;
+		i++;
+		return (i);
+	}
+	return (0);
+}
+
+/**
+ * _strcpy - Function copies a string from a source to a destination
+ * @src: Source of string
+ * @dest: destination of string
+ *
+ * Return: Pointer to the destination
+ */
+void _strcpy(char *dest, char *src)
+{
+	int i;
+	int len_src = _strlen(src);
+
+	if (dest != NULL && src != NULL)
+	{
+		for (i = 0; i < len_src; i++)
+			dest[i] = src[i];
+	}
 }
