aae8e68e127e680571cbd0c70cc058253aaaa6c3
mb <michaelsenapon@gmail.com>
2023-07-05 05:00:36 +0000

    Add void

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * print_pre_main - Function print text before main execute
  */
-void __attribute__((constructor)) print_pre_main()
+void __attribute__((constructor)) print_pre_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
