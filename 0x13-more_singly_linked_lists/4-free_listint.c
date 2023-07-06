2115b9de0439a59690aeba70148dda8e517f974e
michaelsenapon <michaelsenapon@gmail.com>
2023-07-06 13:31:07 +0000

    Free allocated memory for linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function that frees a linked list
+ * @head: Pointer to the first node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *rm;
+	listint_t *trav;
+
+	trav = head;
+
+	while (trav->next)
+	{
+		rm = trav;
+		trav = trav->next;
+		free(rm);
+	}
+	free(trav);
+}
