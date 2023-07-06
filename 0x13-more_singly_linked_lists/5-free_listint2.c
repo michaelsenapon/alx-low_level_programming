e42343c0795fa71eff9a3243119cd807dd51912f
michaelsenapon <michaelsenapon@gmail.com>
2023-07-06 17:31:59 +0000

    Free allocated memories of nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that free allocated memories in a linked list
+ * @head: Pointer to pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *rm;
+	listint_t *trav = *head;
+
+	while (trav)
+	{
+		rm = trav;
+		trav = trav->next;
+		free(rm);
+	}
+	*head = NULL;
+}
