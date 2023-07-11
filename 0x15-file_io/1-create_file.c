0707846ee7476179cada0027e0e68f3ebd932d87
michaelsenapon <michaelsenapon@gmail.com>
2023-07-11 15:08:25 +0000

    Fix Betty style error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT|O_RDWR|O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
