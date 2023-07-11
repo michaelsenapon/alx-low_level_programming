8701d44cae4c38d655e69cd596ef88a59a950493
mb <michaelsenapon@gmail.com>
2023-07-11 02:10:08 +0000

    Function that reads a text file and prints to STDOUT_FILENO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file and prints it to STD_OUT
+ * @filename: buffer to read from
+ * @letters: Number of letters to read and print
+ *
+ * Return: Actual number of letters it could read and print
+ * Or 0 otherwise
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int n, r, p;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	p = open(filename, O_RDONLY);
+	if (p == -1)
+		return (0);
+
+	r = read(p, buf, letters);
+	if (r == -1)
+		return (0);
+
+	n = write(STDOUT_FILENO, buf, letters);
+	if (n == -1)
+		return (0);
+
+	free(buf);
+	close(p);
+	return (r);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/* Standard C library headers */
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Function prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
