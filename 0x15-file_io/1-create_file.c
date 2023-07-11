b60ec0758c4fa959177d36fa572949b3b96c94f9
michaelsenapon <michaelsenapon@gmail.com>
2023-07-11 15:06:45 +0000

    Create a file and write into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * create_file - function that creates a file
- * @filename: name of file to be created
- * @text_content: content to be written into file
+ * create_file - Function that creates a file
+ * @filename: Name of the file to create
+ * @text_content: String of characters to write into file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on Success or -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
+	fd = open(filename, O_CREAT|O_RDWR|O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
-
-	if (text_content)
-		write(fd, text_content, strlen(text_content));
-
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	len = strlen(text_content);
+	write(fd, text_content, len);
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Function Prototypes */
 int _putchar(char c);
